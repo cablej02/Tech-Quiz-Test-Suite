b1d964bed48b5cb064524ce601389f61c222ff61
Jason Cable <cablej02@gmail.com>
2025-02-18 23:03:30 -0600

    added more component tests

--- cypress/component/Quiz.cy.tsx
+++ cypress/component/Quiz.cy.tsx
@@ -5,7 +5,8 @@ describe("<Quiz />", () => {
     // run this code before each test
     beforeEach(() => {
         // intercept the GET request to /api/questions and respond with the questions.json fixture
-        cy.intercept("GET", '/api/questions', { fixture: "questions.json" }).as("getQuestions"); 
+        cy.intercept("GET", '/api/questions/random', { fixture: "questions.json" }).as("getQuestions"); 
+
         // mount the Quiz component
         cy.mount(<Quiz />);
     });
@@ -16,15 +17,25 @@ describe("<Quiz />", () => {
 
     it("should start the quiz on button click", () => {
         cy.contains("button","Start Quiz").click();
-        cy.contains("button","Start Quiz").should("not.exist");
 
         // wait for the GET request to /api/questions to complete
         cy.wait("@getQuestions");
 
-        // loading text should not be visible after the questions are loaded
-        cy.contains("Loading...").should("not.exist");
+        // check if the first question is rendered
+        cy.contains("h2", "Test question 1").should("be.visible");
+    })
+
+    it("should show the next question after answering the current question", () => {
+        cy.contains("button","Start Quiz").click();
+        cy.wait("@getQuestions");
 
         // check if the first question is rendered
-        cy.get("h2").should("exist");
-    })
+        cy.contains("h2", "Test question 1").should("be.visible");
+
+        // click the correct answer
+        cy.contains("correct").prev("button").click();
+
+        // check if the next question is rendered
+        cy.contains("h2", "Test question 2").should("be.visible");
+    });
 });
